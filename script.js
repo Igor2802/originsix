e9db9b8f4503e9eed9eb119c74a593aae7571c80
Igor2802 <61586724+Igor2802@users.noreply.github.com>
2021-07-04 19:07:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//ABRE E FECHA O MENU AO CLICAR NO ICONE//
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL */
+
+
+
+function changeHeaderWhenScroll(){
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+  {
+    if (window.scrollY >= navHeight) {
+      header.classList.add('scroll')
+    } else {
+      header.classList.remove('scroll')
+    }
+  }
+}
+
+/* TESTIMONIALS CAROUSEL SLIDER */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* ScrollReveal: MOSTRAR ELEMENTOS QUANDOR DER SCROOL NA PÁGIAN */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
+
+
+/*MENU ATIVO CONFORME SEÇÃO VISIVEL NA PÁGINA*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for( const section of sections){
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionID = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionID + ']')
+      .classList.add('active')
+    } else{
+      document.querySelector('nav ul li a[href*=' + sectionID + ']')
+      .classList.remove('active')
+    }
+  }
+}
+
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  activateMenuAtCurrentSection()
+})
